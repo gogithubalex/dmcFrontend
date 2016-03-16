e55938ae91be2f2827cfcafa309d9ea84d51a90c
Jim Barkley <james.barkley@gmail.com>
2016-03-16 09:02:04 -0400

    - finished S3 file upload logic  - reports errors  - logs progress in console  - sets result in $scope variables for passing onto rest services

--- app/scripts/onboarding/controllers/profile.js
+++ app/scripts/onboarding/controllers/profile.js
@@ -9,12 +9,22 @@ angular.module('dmc.onboarding')
         $scope.file = null;
 
 
-//upload file
+        //profile pic upload parameters for S3
 	$scope.creds = {
-	bucket: 'dmc-test',
-	access_key: 'AKIAI3UDZVKQDH2E5R5A',
-	secret_key: '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'
+	    bucket: 'dmc-uploads2',
+
+// james.barkley creds (used for testing)
+            access_key: 'AKIAJHHCWWBAHZPREBCQ', 
+            secret_key: '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'
+
+// frontend-uploader creds
+/*
+	    access_key: 'AKIAIYFNN5QQETHXQ5AA',
+    	    secret_key: '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'
+*/
 	}
+
+
  
 
         $scope.prevPicture = null;
@@ -69,72 +79,67 @@ angular.module('dmc.onboarding')
         }
 
         $scope.next = function(index){
-	    file = $scope.file.files[0];
-//	    alert(file.size);
-//	    alert(file.name);
-	    if(file.size > 10585760) {
-	      alert('Sorry, file size must be under 10MB');
-	      return false;
-	    }
-
-  // Configure The S3 Object
-  AWS.config.update({ accessKeyId: $scope.creds.access_key, secretAccessKey: $scope.creds.secret_key });
-  AWS.config.region = 'us-east-1';
-  var bucket = new AWS.S3({ params: { Bucket: $scope.creds.bucket } });
- 
-  if(file) {
-    var params = { Key: file.name, ContentType: file.type, Body: file.file, ServerSideEncryption: 'AES256' };
- 
-    bucket.putObject(params, function(err, data) {
-      if(err) {
-        // There Was An Error With Your S3 Config
-        alert(err.message);
-        return false;
-      }
-      else {
-        // Success!
-        alert('Upload Done');
-      }
-    })
-    .on('httpUploadProgress',function(progress) {
-          // Log Progress Information
-          console.log(Math.round(progress.loaded / progress.total * 100) + '% done');
-        });
-  }
-  else {
-    // No File Selected
-    alert('No File Selected');
-  }
-
-
-
-        }
-/*
-        $scope.next = function(index){
             $scope.storefront[index].done = true;
             if(index == 1 && $scope.file){
-                fileUpload.uploadFileToUrl(
-                    $scope.file.files[0].file,
-                    {id: $scope.userData.profileId}, 
-                    'profile', 
-                    function(data){
-                        $scope.file = null;
-                        if(data.file && data.file.name){
-                            $scope.profile[1].data.image = data.file.name;
+	        file = $scope.file.files[0];
+    	        console.log("file size: " + file.size);
+	        console.log("file name: " + file.name);
+	        if(file.size > 10585760) {
+	            alert('Sorry, file size must be under 10MB');
+	            return false;
+	        }
+
+                // Configure The S3 Object
+                AWS.config.update({ accessKeyId: $scope.creds.access_key, secretAccessKey: $scope.creds.secret_key });
+                AWS.config.region = 'us-east-1';
+                var bucket = new AWS.S3({ params: { Bucket: $scope.creds.bucket } });
+ 
+                if (file) {
+                    var params = { Key: file.name, ContentType: file.type, Body: file.file, ServerSideEncryption: 'AES256' };
+                    bucket.putObject(params, function(err, data) {
+                        if (err) {
+                            // There Was An Error With Your S3 Config
+                            alert(err.message);
+                            return false;
                         }
-                        $scope.saveProfile($scope.profile[index].data, function(){
-                            $(window).scrollTop(0);
-                            $state.go('^' + $scope.profile[index+1].state);
-                        });
-                    }
-                );
+                        else {
+                            // Success!
+                            //alert('Upload Done');
+			    console.log('Upload Done');
+                            $scope.file = null;
+                            $scope.profile[1].data.image = "https://dmc-uploads.s3.amazonaws.com/" + file.name;
+			    console.log("final resource at " + $scope.profile[1].data.image);
+                        }
+                    })
+                   .on('httpUploadProgress',function(progress) {
+                       // Log Progress Information
+                       console.log(Math.round(progress.loaded / progress.total * 100) + '% done');
+                   });
+               }
+               else {
+                   // No File Selected
+                   alert('No File Selected');
+               }
+
             }else{
                 $scope.saveProfile($scope.profile[index].data, function(){
                     $(window).scrollTop(0);
                     $state.go('^' + $scope.profile[index+1].state);
                 });
             }
+
         }
+/*
+                    $scope.file.files[0].file,
+                    {id: $scope.userData.profileId}, 
+                    'profile', 
+                    function(data){
+                        $scope.saveProfile($scope.profile[index].data, function(){
+                            $(window).scrollTop(0);
+                            $state.go('^' + $scope.profile[index+1].state);
+                        });
+                    }
+                );
 */
 
         $scope.finish = function(index){
